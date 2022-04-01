68d8e32698863ec8b8c3fbac61967096f916331d
Cameron Baker <33822366+cambakr@users.noreply.github.com>
2022-04-01 16:11:57 -0700

    Create HomeController.java

---
+++ RestaurantApplication/src/main/java/com/team15/restaurantapplication/controllers/HomeController.java
@@ -0,0 +1,39 @@
+package com.team15.restaurantapplication.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class HomeController {
+    @FXML
+    private Button checkoutButton;
+
+    @FXML
+    private Button homeButton;
+
+    @FXML
+    private Button menuButton;
+
+    @FXML
+    private Button profileButton;
+
+    @FXML
+    void checkoutClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void homeClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void menuClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void profileClicked(ActionEvent event) {
+
+    }
+}
