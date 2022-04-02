1ddb85b62436e916e46f047df7f682352bbd0951
Cameron Baker <33822366+cambakr@users.noreply.github.com>
2022-04-01 17:35:48 -0700

    Create MenuController.java

---
+++ RestaurantApplication/src/main/java/com/team15/restaurantapplication/controllers/MenuController.java
@@ -0,0 +1,66 @@
+package com.team15.restaurantapplication.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+
+public class MenuController {
+
+    @FXML
+    private CheckBox calorieCheckbox;
+
+    @FXML
+    private Button checkoutButton;
+
+    @FXML
+    private Button homeButton;
+
+    @FXML
+    private Button menuButton;
+
+    @FXML
+    private Button profileButton;
+
+    @FXML
+    private Button refreshButton;
+
+    @FXML
+    private TextField searchText;
+
+    @FXML
+    private CheckBox spicyCheckbox;
+
+    @FXML
+    private CheckBox veganCheckbox;
+
+    @FXML
+    private CheckBox vegetarianCheckbox;
+
+    @FXML
+    void checkoutClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void homeClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void menuClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void profileClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void refreshClicked(ActionEvent event) {
+
+    }
+
+}
