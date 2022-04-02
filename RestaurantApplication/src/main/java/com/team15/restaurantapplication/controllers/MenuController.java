1a5ebfb36109fd44a53e1f1e273031baeef0c9c9
mohamedAlmukhtar <mohamedalmukhtar870@gmail.com>
2022-04-01 22:07:21 -0700

    Add menu items grid to Menu page

--- RestaurantApplication/src/main/java/com/team15/restaurantapplication/controllers/MenuController.java
+++ RestaurantApplication/src/main/java/com/team15/restaurantapplication/controllers/MenuController.java
@@ -1,15 +1,30 @@
 package com.team15.restaurantapplication.controllers;
 
 import com.team15.restaurantapplication.RestaurantApplication;
+import com.team15.restaurantapplication.classes.MenuItem;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.text.Text;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class MenuController {
+public class MenuController implements Initializable {
 
     @FXML
     private CheckBox calorieCheckbox;
@@ -40,6 +55,54 @@ public class MenuController {
 
     @FXML
     private CheckBox vegetarianCheckbox;
+
+    @FXML
+    private GridPane menu;
+
+    @FXML 
+    private ScrollPane pane;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+        int column = 0;
+        int row = 1;
+
+        try {
+            
+            for(int i=0; i < 10; i++){
+                FXMLLoader fxmlloader = new FXMLLoader();
+                fxmlloader.setLocation(RestaurantApplication.class.getResource("menuitem.fxml"));
+                AnchorPane menuItem = fxmlloader.load();
+                
+                MenuItemController itemController = fxmlloader.getController();
+                itemController.setData(new MenuItem("Banana", 3.99, "Banana description",
+                        null, null, RestaurantApplication.class.getResource("images.jpg").toExternalForm()));
+            
+            
+                if(column == 3){
+                    column = 0;
+                    row++;
+                }
+
+                menu.add(menuItem, column++, row);
+                menu.setMinWidth(Region.USE_COMPUTED_SIZE);
+                menu.setPrefWidth(Region.USE_COMPUTED_SIZE);
+                menu.setMaxWidth(Region.USE_PREF_SIZE);
+
+                menu.setMinHeight(Region.USE_COMPUTED_SIZE);
+                menu.setPrefHeight(Region.USE_COMPUTED_SIZE);
+                menu.setMaxHeight(Region.USE_PREF_SIZE);
+
+                GridPane.setMargin(menuItem, new Insets(10));
+
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+    }
 
     @FXML
     void checkoutClicked(ActionEvent event) {
--- RestaurantApplication/src/main/java/com/team15/restaurantapplication/controllers/MenuItemController.java
+++ RestaurantApplication/src/main/java/com/team15/restaurantapplication/controllers/MenuItemController.java
@@ -1,31 +1,49 @@
 package com.team15.restaurantapplication.controllers;
 
+import com.team15.restaurantapplication.RestaurantApplication;
+import com.team15.restaurantapplication.classes.MenuItem;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 
 public class MenuItemController {
     
     @FXML
-    private Text name;
+    private Label name;
 
     @FXML
     private Text description;
 
     @FXML
-    private Text price;
+    private Label price;
     
     @FXML
     private ImageView image;
 
     @FXML
-    private Button addToCart;
+    private Button addToCartBtn;
+
+    private MenuItem item;
+
+    public void setData(MenuItem item) {
+        this.item = item;
+        name.setText(item.getName());
+        price.setText("$" + item.getPrice());
+        Image img = new Image(item.getImgPath());
+        
+        image.setImage(img);
+
+
+    }
 
     @FXML
     void addToCartClicked(ActionEvent event) {
-        
+
     }
 
 }
