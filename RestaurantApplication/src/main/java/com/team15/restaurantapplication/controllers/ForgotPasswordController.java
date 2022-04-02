5e862314f7e71be127528e2cd11db6347e346150
Cameron Baker <33822366+cambakr@users.noreply.github.com>
2022-04-01 20:57:31 -0700

    Added partial submit functionality

--- RestaurantApplication/src/main/java/com/team15/restaurantapplication/controllers/ForgotPasswordController.java
+++ RestaurantApplication/src/main/java/com/team15/restaurantapplication/controllers/ForgotPasswordController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class ForgotPasswordController {
@@ -21,14 +22,25 @@ public class ForgotPasswordController {
     private TextField username;
 
     @FXML
+    private Text noUsernameErrorMsg;
+
+    @FXML
     void cancelClicked(ActionEvent event) {
         Stage stage = (Stage) cancelButton.getScene().getWindow();
         stage.close();
     }
 
     @FXML
-    void submitClicked(ActionEvent event) {
-        System.out.println("\"" + username.getText() + "\"");
+    void submitClicked(ActionEvent event) { // Need to search for
+        noUsernameErrorMsg.setVisible(false);
+        if (!username.getText().isEmpty()) { // If a username was input
+            // Search for the user
+            // If they exist, update password
+            // Otherwise, output error
+        } else { // If there was no username input
+            System.out.println("\"" + username.getText() + "\"");
+            noUsernameErrorMsg.setVisible(true);
+        }
     }
 
 }
