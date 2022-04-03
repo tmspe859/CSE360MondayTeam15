fefd1509a075975955119dd36b307b90d6405db7
Cameron Baker <33822366+cambakr@users.noreply.github.com>
2022-04-03 16:02:38 -0700

    Added menu item edit and add popup views

---
+++ RestaurantApplication/src/main/java/com/team15/restaurantapplication/controllers/editMenuItemPopupController.java
@@ -0,0 +1,47 @@
+package com.team15.restaurantapplication.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+
+public class editMenuItemPopupController {
+
+    @FXML
+    private CheckBox calorieCheckbox;
+
+    @FXML
+    private TextField description;
+
+    @FXML
+    private TextField imagePath;
+
+    @FXML
+    private TextField ingredients;
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private TextField price;
+
+    @FXML
+    private CheckBox spicyCheckbox;
+
+    @FXML
+    private CheckBox veganCheckbox;
+
+    @FXML
+    private CheckBox vegetarianCheckbox;
+
+    @FXML
+    void cancelClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editClicked(ActionEvent event) {
+
+    }
+
+}
