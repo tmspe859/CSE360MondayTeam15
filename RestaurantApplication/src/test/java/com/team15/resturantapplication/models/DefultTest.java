7448f1626dc1e4f2313995278995f4526c35c4c4
tmspe859 <71113694+tmspe859@users.noreply.github.com>
2022-04-22 02:25:34 -0700

    Uploaded test file

---
+++ RestaurantApplication/src/test/java/com/team15/resturantapplication/models/DefultTest.java
@@ -0,0 +1,19 @@
+package com.team15.resturantapplication.models;
+import com.team15.restaurantapplication.models.*;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DefultTest {
+    
+    @Test
+    public void Test_Model() {
+        System.out.println("The default configuration");
+    }
+
+    @Test
+    public void test2() {
+        assertTrue(true);
+    }
+}
